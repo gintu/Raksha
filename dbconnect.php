09a05823a9c02b4446e85c5913544e6e14526a04
azin.makaranth <azin.makaranth@gmail.com>
2018-03-17 02:12:40 +0530

    dbConnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,12 @@
 <?php
-$serverName = "rakshadb.database.windows.net";
-$connectionOptions = array(
-    "Database" => "rakshadb",
-    "Uid" => "raksha1",
-    "PWD" => "gintutom@1"
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if ($conn)
-echo "Connection Successful";
-else
-echo "Connection Fail";
-?>
+$servername = "localhost";
+$username = "root";
+$password = "123";
+$db = "rakshadb";
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
