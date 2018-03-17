a1ba16898fbba6febe98268f69196fc2c7ac52fa
abhi1401 <abhishek.mvr@outlook.com>
2018-03-17 17:54:02 +0530

    edit in emergency

--- project/emergency.php
+++ project/emergency.php
@@ -5,32 +5,10 @@ include_once("dbconnect.php");
 include_once("header.php");
 $lat = $_POST["lat"];
 $lng = $_POST["lng"];
-$tasklist = "select * from tasks where lat like ".$lat." and lng like ".$lng." and date like CURDATE()";
-//echo $tasklist;
-$itasklist = mysqli_query($con,$tasklist);
-if (!isset($itasklist)) {
-
-    $dbupdate="insert into tasks(lat,lng,date) values(".$lat.",".$lng.",CURDATE())";
-    //echo $dbupdate;
-    mysqli_query($con,$dbupdate);
-    $query="SELECT * from volunteer where task IS NULL";
-    //echo $query;
-    $result = mysqli_query($con,$query);
-    if ($result) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            if((distance($lat,$lng,$row['lat'],$row['lng']))<20)
-            {
-
-            }
-        }
-    } else {
-        echo "0 results";
-    }
-
-}
-else
-echo "Task already issued. We're on it.";
+$dbupdate="insert into tasks(lat,lng,date) values(".$lat.",".$lng.",CURDATE())";
+//echo $dbupdate;
+mysqli_query($con,$dbupdate);
 header("location:panicbutton.php");
+
 mysqli_close($con);
 ?>
