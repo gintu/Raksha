da22d2ec0bc2a3d21216d32ff572cc59aa271278
azin.makaranth <azin.makaranth@gmail.com>
2018-03-17 09:37:28 +0530

    Volreg...

--- project/volregAction.php
+++ project/volregAction.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'dbconnect.php';
 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if($_POST['name'] != "" && $_POST['email']!="" && $_POST['mobile'] != "" && $_POST['password']!= "" && $_POST['location']!="" && $_POST['abt']!="" ){
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -22,4 +24,46 @@ else
   echo "Fail";
 }
 else
+<<<<<<< HEAD
+   echo "<body style=\"background-color:#c0392b;color:#3498db;\"><center>Missing Inputs<br><a href =\"index.html\">SafePlace</a></center>";
+}
+if(isset($_GET['mob'])){
+    $m = $_GET['mob'];
+    $q = "select * from volunteer where mobile = ".$m."";
+    $res = mysqli_query($con,$q);
+
+     if($res){
+        $res_num = mysqli_num_rows($res);
+        if($res_num >= 1){
+            //$row=mysqli_fetch_assoc($res);
+            echo 'Already Exists';
+        }
+        else{
+            echo '';
+        } 
+     }
+     else{
+        echo '';   
+     }
+}
+if(isset($_GET['email'])){
+  $m = $_GET['email'];
+  $q = "select * from volunteer where email = '".$m."'";
+  $res = mysqli_query($con,$q);
+   if($res){
+      $res_num = mysqli_num_rows($res);
+      if($res_num >= 1){
+          //$row=mysqli_fetch_assoc($res);
+          echo 'Already Exists';
+      }
+      else{
+          echo '';
+      } 
+   }
+   else{
+      echo '';   
+   }
+}
+=======
  echo "<body style=\"background-color:#c0392b;color:#3498db;\"><center>Missing Inputs<br><a href =\"index.php\">SafePlace</a></center>";
+>>>>>>> 5244e0b4c4a2c927a24e9e1c11beb33851e7a89c
